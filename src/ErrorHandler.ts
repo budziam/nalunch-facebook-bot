181be2864951d031b00782a6ae52831b51b8e5ab
Michał Budziak <michal.mariusz.b@gmail.com>
2019-07-12 10:39:30 +0200

    Better axios error handling

--- src/ErrorHandler.ts
+++ src/ErrorHandler.ts
@@ -3,17 +3,16 @@ import { Request, Response } from "express";
 import { injectable } from "inversify";
 import * as winston from "winston";
 import * as http2 from "http2";
-const { HTTP_STATUS_INTERNAL_SERVER_ERROR, HTTP_STATUS_NOT_FOUND } = http2.constants;
 import { EndpointNotFoundError } from "./http/errors";
 import { TERMINATION } from "./http/utils";
+import { isAxiosError } from "./exceptions/utils";
+import { AxiosError } from "axios";
+
+const { HTTP_STATUS_INTERNAL_SERVER_ERROR, HTTP_STATUS_NOT_FOUND } = http2.constants;
 
 @boundClass
 @injectable()
 export class ErrorHandler {
-    public handle(error: any): void {
-        winston.error(error);
-    }
-
     public handleHttpError(e: any, req: Request, res: Response): Response | undefined {
         if (e === TERMINATION) {
             return undefined;
@@ -23,8 +22,33 @@ export class ErrorHandler {
             return res.sendStatus(HTTP_STATUS_NOT_FOUND);
         }
 
-        winston.error(e);
+        this.handle(e);
 
         return res.sendStatus(HTTP_STATUS_INTERNAL_SERVER_ERROR);
+    }
+
+    public handle(error: any): void {
+        if (isAxiosError(error)) {
+            const axiosError: AxiosError = error;
+
+            winston.error("Request error", {
+                code: axiosError.code,
+                request: {
+                    method: axiosError.config.method,
+                    url: axiosError.config.url,
+                    params: axiosError.config.params,
+                    data: axiosError.config.data,
+                    headers: axiosError.config.headers,
+                },
+                response: axiosError.response && {
+                    data: axiosError.response.data,
+                    status: axiosError.response.status,
+                    statusText: axiosError.response.statusText,
+                    headers: axiosError.response.headers,
+                },
+            });
+        } else {
+            winston.error(error);
+        }
     }
 }
