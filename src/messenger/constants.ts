d307223b143a1453a4792f0ad9f2273afcad8595
Michał Budziak <michal.mariusz.b@gmail.com>
2019-07-12 10:53:56 +0200

    Use another image

--- src/messenger/constants.ts
+++ src/messenger/constants.ts
@@ -12,6 +12,7 @@ export const ACTION_CHOICE_REPLIES: QuickReply[] = [
         content_type: ContentType.Text,
         title: "Rozmowa z człowiekiem",
         payload: ActionChoicePayload.Conversation,
-        image_url: "https://static.xx.fbcdn.net/images/emoji.php/v9/t8f/1/16/1f469.png",
+        image_url:
+            "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/198/woman_1f469.png",
     },
 ];
